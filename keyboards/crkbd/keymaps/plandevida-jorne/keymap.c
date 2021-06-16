13bb2820f53ab9b7eec18ffec897a2aad697bb54
Daniel Serrano <plandevida@users.noreply.github.com>
2021-06-16 15:45:22 +0200

    Add modifier RALT to LBRC and RBRC
    Iso keymaps  can write square brackets

--- keyboards/crkbd/keymaps/plandevida-jorne/keymap.c
+++ keyboards/crkbd/keymaps/plandevida-jorne/keymap.c
@@ -118,7 +118,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(\
-    KC_RGUI, KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, 	          KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC,       KC_RBRC, \
+    KC_RGUI, KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, 	          KC_Y, KC_U, KC_I,    KC_O,   KC_P,    ALGR(KC_LBRC),       ALGR(KC_RBRC), \
              KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, 		      KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
              KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, 		      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MT_RS_BSLS, \
                           LT_MSE_DEL, LT_NUM_SPC, KC_ENT,    KC_ENT, LT_NUM_SPC, MT_LALT_BSPC \
